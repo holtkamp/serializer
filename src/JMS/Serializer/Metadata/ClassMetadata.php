ceb33d8fd9d980ea8ecc8669dc216189b547234a
Menno Holtkamp <menno.holtkamp@weev.nl>
2014-01-20 00:55:54 +0100

    Respect already configured discriminator related settings, after which they can be processed

--- src/JMS/Serializer/Metadata/ClassMetadata.php
+++ src/JMS/Serializer/Metadata/ClassMetadata.php
@@ -158,6 +158,13 @@ class ClassMetadata extends MergeableClassMetadata
             ));
         }
 
+        //Respect already configured discriminator related settings, after which they can be processed
+        $this->discriminatorDisabled = $object->discriminatorDisabled;
+        $this->discriminatorBaseClass = $object->discriminatorBaseClass;
+        $this->discriminatorFieldName = $object->discriminatorFieldName;
+        $this->discriminatorValue = $object->discriminatorValue;
+        $this->discriminatorMap = array_merge($this->discriminatorMap, $object->discriminatorMap);
+
         if ($this->discriminatorMap && ! $this->reflection->isAbstract()) {
             if (false === $typeValue = array_search($this->name, $this->discriminatorMap, true)) {
                 throw new \LogicException(sprintf(
